f5f81731e8b7e9a232c1858e20dc58c798a49cd6
purdenko_konstantine <eduardpur18@gmail.com>
2020-06-21 16:10:24 +0300

    no message

---
+++ blocks/head.php
@@ -0,0 +1,21 @@
+<div id="basic">
+     
+	 
+        <div id="head-site">
+            <a href="#">
+			<img src="img/logo.png" width="10%" height="7%" alt="Логотип автосалона" title="Логотип автосалона" /></a>
+			<img src="img/auto_logo.png" width="22%" height="15%" alt="Автосалон" title="Автосалон"/></a>
+			
+           
+			
+			
+    <div id="menu-top">
+        <div class="bg-1">
+            <ul>
+            <li><a href="about_us.html">О нас</a></li>
+            <li><a href="index.html">Перечень автомобилей</a></li>
+            <li class="none-bg"><a href="contact.html">Контактная информация</a></li>
+            </ul>
+        </div> 	
+        <div class="bg-2"></div>
+    </div>
